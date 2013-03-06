4e2f33632917b125d415524dc45e15d3f259b730
631220037 <631220037@Sala603-15.fspoaeduc.com.br>
2013-03-05 22:17:03 -0300

    Trabalho finalizado

--- entregas/trabalho2/src/main/java/br/com/senacrs/alp/aulas/Somatorio.java
+++ entregas/trabalho2/src/main/java/br/com/senacrs/alp/aulas/Somatorio.java
@@ -14,7 +14,14 @@ public class Somatorio {
 	public double somaTotal(double[] valores) {
 		
 		double resultado = 0.0;
-		
+		if (valores == null){
+			resultado = Double.NaN;
+		}
+		else{
+		for (int i=0;i<valores.length;i++){
+			resultado = valores[i]+resultado;
+		}
+		}
 		//Implementar
 		
 		return resultado;

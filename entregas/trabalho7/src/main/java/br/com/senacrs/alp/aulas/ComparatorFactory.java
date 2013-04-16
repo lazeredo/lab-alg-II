d586d9c5220e5e23a6beaf875c2610d89632ff3b
LUCAS <LUCAS@192.168.2.3>
2013-04-15 23:34:42 -0300

    trabalho7

---
+++ entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/ComparatorFactory.java
@@ -0,0 +1,33 @@
+package br.com.senacrs.alp.aulas;
+
+import java.util.Comparator;
+
+public class ComparatorFactory {
+	
+	private static final ComparatorFactory instancia = new ComparatorFactory();
+	
+	private ComparatorFactory() {
+	}
+	
+	public Comparator<Integer> criarComparatorParesAntesDeImparesCrescente() {
+		
+		Comparator<Integer> resultado = null;
+		
+		resultado = new MeuComparatorINT();
+		
+		return resultado;
+	}
+	
+	public Comparator<String> criarComparatorOrdemAlfabeticaReversa() {
+		
+		Comparator<String> resultado = null;
+		
+		resultado = new MeuComparatorSTRING();
+		
+		return resultado;
+	}
+	
+	public static ComparatorFactory getInstancia() {
+		return instancia;
+	}
+}
---
+++ entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparatorSTRING.java
@@ -0,0 +1,21 @@
+package br.com.senacrs.alp.aulas;
+
+import java.util.Comparator;
+
+public class MeuComparatorSTRING implements Comparator<String> {
+
+	@Override
+	public int compare(String o1, String o2) {
+		
+		if(o1 == null && o2 == null){
+			return 0;
+		}else if(o1 != o2){
+			return -1;
+		}else if(o1 == o2){
+			return 1;
+		}else{
+			return o2.compareTo(o1);
+		}
+	}
+
+}
